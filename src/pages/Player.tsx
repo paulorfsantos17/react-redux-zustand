b2aae09718b4deef649ae3ae0106e50321ccb5d6
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-08 15:02:41 -0300

    feat: ensure title in document show name lesson

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,11 +3,17 @@ import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
 import { useSelectorApp } from "../store";
+import { useCurrentLesson } from "../store/slices/player";
+import { useEffect } from "react";
 
 export default function Player() {
 
   const modules = useSelectorApp (state => state.player.course.modules)
 
+  const {currentLesson} = useCurrentLesson()
+  useEffect(() =>  {
+    document.title = currentLesson.title
+  }, [currentLesson])
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
