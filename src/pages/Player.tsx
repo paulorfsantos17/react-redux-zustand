4ada14c4c44401c80db01abcc7c18edc254f7816
paulorfsantos17 <pauorfsantos17@gmail.com>
2024-10-08 14:19:42 -0300

    feat: ensure get Modules course in store

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -2,8 +2,13 @@ import {  MessageCircle } from "lucide-react";
 import { Header } from "../components/Header";
 import { Video } from "../components/Video";
 import { Module } from "../components/Module";
+import { useSelectorApp } from "../store";
 
 export default function Player() {
+
+  const modules = useSelectorApp (state => state.player.course.modules)
+
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1100px] flex-col gap-6">
@@ -23,9 +28,14 @@ export default function Player() {
           <aside className="w-80 absolute top-0 bottom-0  right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 
             overflow-y-auto  scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800"
           >
-            <Module title="Fundamentos do Redux" amountOfLessons={12} moduleIndex={0}/>
-            <Module title="Fundamentos do Redux" amountOfLessons={12} moduleIndex={1}/>
-            <Module title="Fundamentos do Redux" amountOfLessons={12} moduleIndex={2}/>
+            {modules.map((module, index) => {
+              return <Module 
+                key={module.id} 
+                title={module.title} 
+                amountOfLessons={module.lessons.length} 
+                moduleIndex={index}
+              />
+            })}
           </aside>
         </main>
       </div>
